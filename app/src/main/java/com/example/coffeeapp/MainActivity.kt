e33c7ff150fb330ce74683ab893528a54a73919a
gülce <gizemgulce@gmail.com>
2021-09-02 23:22:42 +0300

    data

--- app/src/main/java/com/example/coffeeapp/MainActivity.kt
+++ app/src/main/java/com/example/coffeeapp/MainActivity.kt
@@ -1,28 +1,33 @@
 package com.example.coffeeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     var total: Double = 10.0
     var lastTotal: Double = 0.0
+    lateinit var radioButton: RadioButton
+    var checkedExtras = ArrayList<String>()
+    var number: Int=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("----------------------")
+
 
         val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
         val price: TextView = findViewById(R.id.price)
         val seek = findViewById<SeekBar>(R.id.seekBar)
+
         var checked1: CheckBox = findViewById(R.id.sugarbutton)
         var checked2: CheckBox = findViewById(R.id.creambutton)
         var checked3: CheckBox = findViewById(R.id.nutbutton)
         var checked4: CheckBox = findViewById(R.id.mintbutton)
-
-
 
         radioGroup.setOnCheckedChangeListener { group, checkedId ->
 
@@ -121,7 +126,7 @@ class MainActivity : AppCompatActivity() {
 
 
             checked1.setOnCheckedChangeListener { buttonView, isChecked ->
-
+            checkedExtras.add(checked1.text as String)
                 total += 0.0
                 println("3: $total")
 
@@ -130,6 +135,7 @@ class MainActivity : AppCompatActivity() {
 
             }
             checked2.setOnCheckedChangeListener { buttonView, isChecked ->
+                checkedExtras.add(checked2.text as String)
 
                 total += 0.0
                 println("3: $total")
@@ -139,8 +145,9 @@ class MainActivity : AppCompatActivity() {
 
             }
             checked3.setOnCheckedChangeListener { buttonView, isChecked ->
+                checkedExtras.add(checked3.text as String)
 
-                println("durum : " +checked4.isChecked)
+                println("durum : " + checked4.isChecked)
 
                 if (checked3.isChecked) {
                     println(checked4.isChecked)
@@ -164,9 +171,9 @@ class MainActivity : AppCompatActivity() {
 
 
             }
-
             checked4.setOnCheckedChangeListener { buttonView, isChecked ->
-                println("durum : " +checked4.isChecked)
+                println("durum : " + checked4.isChecked)
+                checkedExtras.add(checked4.text as String)
 
                  if (checked4.isChecked) {
                      println(checked4.isChecked)
@@ -192,23 +199,19 @@ class MainActivity : AppCompatActivity() {
             }
 
 
-
-
-
             seek?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                     val seekBarTexti: TextView = findViewById(R.id.num)
                     seekBarTexti.setText("Number: $p1")
-                    lastTotal =total.times(p1)
+                    lastTotal = total.times(p1)
+                    number= p1
 
                     price.text = lastTotal.toString()
 
-                    println("//////////////////$lastTotal")
 
                 }
 
                 override fun onStartTrackingTouch(seek: SeekBar) {
-                    // write custom code for progress is started
                 }
 
                 override fun onStopTrackingTouch(seek: SeekBar) {
@@ -221,6 +224,29 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-}
 
+    fun submit(view: View){
+        val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
+        val selectedOption: Int = radioGroup.checkedRadioButtonId
+        var name: EditText=findViewById(R.id.customerName)
+        val price: TextView = findViewById(R.id.price)
+
+        val intent= Intent(applicationContext, SecondPage::class.java)
+        radioButton = findViewById(selectedOption)
+        for( item in checkedExtras ){
+            print("5555    ")
+            println(item)
+
+        }
+        intent.putExtra("customer_name", name.text.toString())
+        intent.putExtra("selectedSize", radioButton.text)
+        intent.putExtra("selectedExtras", checkedExtras)
+        println("number:  ${number}")
+        intent.putExtra("totalPrice", price.text)
+        intent.putExtra("number", number.toString())
+
+        startActivity(intent)
+
+    }
+}
 
--- app/src/main/java/com/example/coffeeapp/SecondPage.kt
+++ app/src/main/java/com/example/coffeeapp/SecondPage.kt
@@ -2,10 +2,31 @@ package com.example.coffeeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.TextView
 
 class SecondPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_page)
+        var name: TextView = findViewById(R.id.name)
+        var drinkSize: TextView = findViewById(R.id.size)
+        var extra:TextView=findViewById(R.id.extras)
+        var priceTotal: TextView=findViewById(R.id.textView6)
+        var numDrink: TextView=findViewById(R.id.number)
+
+
+
+        val intent=intent
+        val customer= intent.getStringExtra("customer_name")
+        val size= intent.getStringExtra("selectedSize")
+        val extras= intent.getStringArrayListExtra("selectedExtras")
+        val total=intent.getStringExtra("totalPrice")
+        val num=intent.getStringExtra("number")
+        name.text=customer
+        drinkSize.text=size
+        extra.text= extras.toString()
+        priceTotal.text=total
+        numDrink.text=num
     }
 }
